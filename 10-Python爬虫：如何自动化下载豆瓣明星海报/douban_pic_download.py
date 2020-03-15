f669740f021c6a098163bc21199b73e6a1e080bd
LearningChanging <cool_kj@126.com>
2020-03-15 18:35:42 +0800

    Python3 requests.get() 方法 要设置：headers

--- 10-Python爬虫：如何自动化下载豆瓣明星海报/douban_pic_download.py
+++ 10-Python爬虫：如何自动化下载豆瓣明星海报/douban_pic_download.py
@@ -1,7 +1,5 @@
-from urllib.request import urlopen, Request
 import requests
 import json
-import urllib
 
 query = '葛优'
 
@@ -14,8 +12,6 @@ def download(src, id):
     dir = './' + str(id) + '.jpg'
     save = False
     try:
-        # 爬虫注意休息，当心反爬
-        # 测试图片链接没有被网站设置反爬
         pic = requests.get(src, timeout=10)
         save = True
     except requests.exceptions.ConnectionError:
@@ -26,14 +22,13 @@ def download(src, id):
         fp.close()
 
 
-# 下载200张得了，别爬的太猛
-for i in range(0, 200, 20):
+# 下载80张得了，别爬的太猛
+for i in range(0, 80, 20):
     # 'https://www.douban.com/j/search_photo?q=%E8%91%9B%E4%BC%98&limit=20&start=0'
     # 对中文进行转码 urllib.parse.quote(query)
-    url = 'https://www.douban.com/j/search_photo?q=' + urllib.parse.quote(query) + '&limit=20&start=' + str(i)
-    # 上面的链接被网站设置反爬 requests.get(url) 已经无法使用
-    html = Request(url, headers=headers)
-    res = urlopen(html).read()
+    url = 'https://www.douban.com/j/search_photo?q=' + query + '&limit=20&start=' + str(i)
+    html = requests.get(url, headers=headers)
+    res = html.text
     response = json.loads(res)
     for image in response['images']:
         print(image['src'])
